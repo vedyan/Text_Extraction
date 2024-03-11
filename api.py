8f2b58fbc3a9d22a1d33667bcbddb272984b532d
keshav0375 <keshavk5655@gmail.com>
2024-03-11 10:59:32 +0530

    html

--- api.py
+++ api.py
@@ -5,6 +5,26 @@ from typing import Any
 
 app = FastAPI()
 
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PDF Text Extractor</title>
+    </head>
+    <body>
+        <div>
+            <h1>PDF Text Extractor</h1>
+            <form action="/extract_text" method="post" enctype="multipart/form-data">
+                <label for="pdf_file">Upload a PDF file:</label><br>
+                <input type="file" id="pdf_file" name="pdf_file"><br><br>
+                <input type="submit" value="Submit">
+            </form>
+            <p>Powered by <a href="https://vercel.com" target="_blank">Vercel</a></p>
+        </div>
+    </body>
+</html>
+"""
+
 
 @app.post("/extract_text")
 async def extract_text(pdf_file: UploadFile = File(...)) -> Any:
