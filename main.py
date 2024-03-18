3ad9abb26d0eecaf87a12794e88d055ff4d042af
keshav0375 <keshavk5655@gmail.com>
2024-03-18 15:28:26 +0530

    utils-api added

--- main.py
+++ main.py
@@ -3,21 +3,9 @@ import fitz
 import requests
 from typing import Any
 import re
-import google.generativeai as genai
-import os
-from dotenv import load_dotenv
+from utils import extract_projects, extract_experience
 
 app = FastAPI()
-
-load_dotenv()
-
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-
-def get_gemini_response(input):
-    model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(input)
-    return response.text
 
 
 def extract_mobile_number(text):
@@ -63,75 +51,6 @@ def extract_skills(text):
             extracted_skills.extend(skills)
 
     return extracted_skills
-
-
-def extract_experience(text):
-    prompt = f"""
-    **Resume Analysis:**
-
-    The provided resume contains the following details:
-    {text}
-
-    As an experienced text extraction model, your task is to accurately extract and organize the relevant information from the resume.
-
-    Please extract the following details and organize them into a structured response as a list with no heading:
-
-    Extract responsibilities or tasks associated with each job with job title and company name.
-
-    Please ensure that the extracted information is accurate and complete. You may use techniques such as named entity recognition, dependency parsing, or any other suitable methods for extraction.
-
-    For each category, return a list containing the extracted information. If no information is found for a particular category, return an empty list.
-
-    Here's an example of how the response should be structured:
-
-    ["software engineer", "oxyz company", ["Developed and maintained web applications", "Implemented RESTful APIs"], "Web Developer", "ABC company" ,["Designed and implemented responsive web interfaces"]]
-
-    """
-    response = get_gemini_response(prompt)
-    return response
-
-
-def extract_projects(text):
-    prompt = f"""
-    **Resume Analysis:**
-
-    The provided resume contains the following projects section:
-    {text}
-
-    As a text extraction model, your task is to accurately extract responsibilities or tasks associated with each project listed under the projects section. For each project, extract the following details:
-
-    1. Project Name
-    2. Role/Position
-    3. Date (Start date - End date, if available)
-    4. Responsibility
-
-    Organize the extracted information into a structured response as a list with no heading. Each item in the list should contain the details for one project.
-
-    Ensure that the extracted information is accurate and complete. You may use techniques such as named entity recognition, dependency parsing, or any other suitable methods for extraction.
-
-    For each project, return a dictionary containing the extracted information. If no information is found for a particular project, skip it in the list.
-
-    Example of the expected response format:
-    [
-        {{
-            "Project Name": "Data Dialect",
-            "Role": "Team Lead",
-            "Date": "1st September 2023 - 18th January 2024",
-            "Responsibility":" Built end-to-end application enabling natural language database access using Google PaLM, LangChain, Chroma DB, streamlit and Hugging Face vector embeddings."
-        }},
-        {{
-            "Project Name": "Cell Vision",
-            "Role": "Data Collector",
-            "Date": "24th June 2021",
-            "Responsibility":"Led cell segmentation project using YOLO v8 for precise instance segmentation. Built user-friendly Flask app deployed on Azure for scalability"
-        }},
-    ]
-
-    Please ensure accurate extraction and proper formatting. If you encounter any difficulties or have any questions, feel free to ask.
-    """
-    response = get_gemini_response(prompt)
-    print(response)
-    return response
 
 
 @app.post("/extract_text")
---
+++ utils.py
@@ -0,0 +1,77 @@
+import google.generativeai as genai
+
+genai.configure(api_key='AIzaSyAqmRO04L7vpSAiAYXGEOBMlStrvajKh40')
+
+
+def get_gemini_response(input):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(input)
+    return response.text
+
+
+def extract_experience(text):
+    prompt = f"""
+    **Resume Analysis:**
+
+    The provided resume contains the following details:
+    {text}
+
+    As an experienced text extraction model, your task is to accurately extract and organize the relevant information from the resume.
+
+    Please extract the following details and organize them into a structured response as a list with no heading:
+
+    Extract responsibilities or tasks associated with each job with job title and company name.
+
+    Please ensure that the extracted information is accurate and complete. You may use techniques such as named entity recognition, dependency parsing, or any other suitable methods for extraction.
+
+    For each category, return a list containing the extracted information. If no information is found for a particular category, return an empty list.
+
+    Here's an example of how the response should be structured:
+
+    ["software engineer", "oxyz company", ["Developed and maintained web applications", "Implemented RESTful APIs"], "Web Developer", "ABC company" ,["Designed and implemented responsive web interfaces"]]
+
+    """
+    response = get_gemini_response(prompt)
+    return response
+
+
+def extract_projects(text):
+    prompt = f"""
+    **Resume Analysis:**
+
+    The provided resume contains the following projects section:
+    {text}
+
+    As a text extraction model, your task is to accurately extract responsibilities or tasks associated with each project listed under the projects section. For each project, extract the following details:
+
+    1. Project Name
+    2. Role/Position
+    3. Date (Start date - End date, if available)
+    4. Responsibility
+
+    Organize the extracted information into a structured response as a list with no heading. Each item in the list should contain the details for one project.
+
+    Ensure that the extracted information is accurate and complete. You may use techniques such as named entity recognition, dependency parsing, or any other suitable methods for extraction.
+
+    For each project, return a dictionary containing the extracted information. If no information is found for a particular project, skip it in the list.
+
+    Example of the expected response format:
+    [
+        {{
+            "Project Name": "Data Dialect",
+            "Role": "Team Lead",
+            "Date": "1st September 2023 - 18th January 2024",
+            "Responsibility":" Built end-to-end application enabling natural language database access using Google PaLM, LangChain, Chroma DB, streamlit and Hugging Face vector embeddings."
+        }},
+        {{
+            "Project Name": "Cell Vision",
+            "Role": "Data Collector",
+            "Date": "24th June 2021",
+            "Responsibility":"Led cell segmentation project using YOLO v8 for precise instance segmentation. Built user-friendly Flask app deployed on Azure for scalability"
+        }},
+    ]
+
+    Please ensure accurate extraction and proper formatting. If you encounter any difficulties or have any questions, feel free to ask.
+    """
+    response = get_gemini_response(prompt)
+    return response
